47c7daa4e01e2267559c10823a755a028d2a3220
Ion <cristeaion36@gmail.com>
2023-06-05 07:33:01 +0300

    fix password update request

--- YugaApp/app/src/main/java/co/icristea/yuga/data/authorization/AuthorisationApi.kt
+++ YugaApp/app/src/main/java/co/icristea/yuga/data/authorization/AuthorisationApi.kt
@@ -3,26 +3,27 @@ package co.icristea.yuga.data.authorization
 import co.icristea.yuga.data.authorization.remote.dto.RestorePasswordDto
 import co.icristea.yuga.data.authorization.remote.dto.UserDto
 import retrofit2.http.Body
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface AuthorisationApi {
 
-    @POST("signup")
+    @POST("user/signup")
     suspend fun signupUser(@Body data: Map<String,String>): UserDto
 
-    @POST("login")
+    @POST("user/login")
     suspend fun loginUser(@Body data: Map<String, String>): UserDto
 
-    @POST("restore")
+    @POST("password/restore")
     suspend fun restorePassword(@Body data: Map<String, String>): RestorePasswordDto
 
-    @POST("verify-code")
+    @POST("/password/verify-code")
     suspend fun verifyCode(@Body data: Map<String, String>)
 
-    @POST("verify-code")
+    @PATCH("password/update")
     suspend fun updatePassword(@Body data: Map<String, String>)
 
     companion object {
-       const val BASE_URL = "http://192.168.1.102:8080/user/"
+       const val BASE_URL = "http://10.0.2.2:8080/"
     }
 }//"http://10.0.2.2:8080/user/" emmulators
